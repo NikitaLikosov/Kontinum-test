69cb026b4139a6632ee871c41d3d277220331291
Никита Лыкосов <73381666+NikitaLikosov@users.noreply.github.com>
2021-06-20 07:01:29 +0500

    add formatted input

--- index.js
+++ index.js
@@ -6,3 +6,45 @@ function clickMenu(e) {
 }
 
 menu.addEventListener('click', clickMenu)
+
+const password = document.querySelector('.main__item[type=password]')
+const tel = document.querySelector('.main__item[type=tel]')
+tel.addEventListener('input', function (e) {
+  if (e.target.value === '+') {
+    return
+  }
+  let input = e.target.value.replace(/\D/g, '')
+  let formattedInputValue = '+7 ('
+  if (!input) {
+    //is null
+    return (e.target.value = '')
+  }
+  if (['7', '8'].includes(input[0])) {
+    //Rusian phones number formatted
+    input = input.substring(1)
+  }
+
+  //add (, ),  -
+  formattedInputValue += input.substring(0, 3)
+  if (input.length >= 4) {
+    formattedInputValue += ') ' + input.substring(3, 6)
+  }
+  console.log(input)
+  if (input.length >= 7) {
+    formattedInputValue += '-' + input.substring(6, 8)
+  }
+  if (input.length >= 9) {
+    formattedInputValue += '-' + input.substring(8, 10)
+  }
+
+  e.target.value = formattedInputValue
+})
+tel.addEventListener('keydown', function (e) {
+  //delete first symbols
+  if (e.target.value === '+7 (' && e.keyCode === 8) {
+    e.target.value = ''
+  }
+})
+password.addEventListener('input', function (e) {
+  e.target.value = e.target.value.replace(/\W/g, '')
+})
